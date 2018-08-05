7798b7cdf19672407543180834cc4b76f252e6e6
GamesTrap <GamesTrap@web.de>
2018-08-05 11:20:12 +0200

    Added SumCalculator

---
+++ SumCalculator/src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+	int summand1, summand2;
+
+	//Read in two numbers
+	std::cout << "Please enter two integers: ";
+	std::cin >> summand1 >> summand2;
+
+	//Calculate the sum of both numbers
+	const int sum = summand1 + summand2;
+
+	//Show the result on the screen
+	std::cout << "Sum = " << sum << std::endl;
+
+	std::cout << "Press any key to continue . . . ";
+	std::cin.ignore(1000, '\n');
+	std::cin.get();
+	return 0;
+}
